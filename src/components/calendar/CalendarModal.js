be3bc8eb364f63913b79534711d4e13c75c257ec
Mauricio <mauriciog87@gmail.com>
2021-04-04 19:04:41 -0300

    getting a better time picker

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,40 +1,106 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment';
 
 const customStyles = {
-    content : {
-      top                   : '50%',
-      left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
     }
 };
 
 Modal.setAppElement('#root');
 
+const now = moment().minutes(0).seconds(0).add(1, 'hours');
+const nowPlusOneHour = now.clone().add(1, 'hours');
+
 export const CalendarModal = () => {
-    const [isOpen, setisOpen] = useState(true);
+    const [dateStart, setDateStart] = useState(now.toDate());
+    const [dateEnd, setDateEnd] = useState(nowPlusOneHour.toDate());
 
     const closeModal = () => {
-        setisOpen(false);
+        //setisOpen(false);
+    }
+
+    const handleStartDateChange = (e) => {
+        setDateStart(e);
+    }
+
+    const handleEndtDateChange = (e) => {
+        setDateEnd(e);
     }
 
     return (
         <Modal
-          isOpen={isOpen}
-        //   onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-          closeTimeoutMS={200}
-          className="modal"
-          overlayClassName="modal-background"
+            isOpen={true}
+            //   onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+            closeTimeoutMS={200}
+            className="modal"
+            overlayClassName="modal-background"
         >
-            <h2>Hello world</h2>
+            <h1>New Event</h1>
             <hr />
-            <span>hi again</span>
+            <form className="container">
+                <div className="form-group">
+                    <label>Start date</label>
+                    <DateTimePicker
+                        onChange={handleStartDateChange}
+                        value={dateStart}
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>End date</label>
+                    <DateTimePicker
+                        onChange={handleEndtDateChange}
+                        minDate={dateStart}
+                        value={dateEnd}
+                        className="form-control"
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group">
+                    <label>Title and Notes</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Event title"
+                        name="title"
+                        autoComplete="off"
+                    />
+                    <small id="emailHelp" className="form-text text-muted">A short description</small>
+                </div>
+
+                <div className="form-group">
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        placeholder="Notes"
+                        rows="5"
+                        name="notes"
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">Other information</small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Save</span>
+                </button>
+
+            </form>
         </Modal>
     )
 }
